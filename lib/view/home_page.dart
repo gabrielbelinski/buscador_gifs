b022cf80f262219b75faeb89b693aa4451a7491c
Gabriel Belinski <gabrielbelinskisalomao@gmail.com>
2024-10-03 09:52:12 -0300

    Commit inicial

---
+++ lib/view/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:buscador_gifs/service/gif_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search = "";
+  int _offset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getAPI(_search, _offset).then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: "Pesquise aqui",
+              labelStyle: TextStyle(color: Colors.white),
+              border: OutlineInputBorder(),
+            ),
+            style: TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+            onSubmitted: (value) {
+              setState(() {
+                _search = value;
+              });
+            },
+          ),
+        ),
+        Expanded(
+            child: FutureBuilder(
+                future: getAPI(_search, _offset),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError) {
+                        return Container();
+                      } else {
+                        return _createGifTable(context, snapshot);
+                      }
+                  }
+                }))
+      ]),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return Container();
+  }
+}
